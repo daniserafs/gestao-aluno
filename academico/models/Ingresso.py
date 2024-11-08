611604608d7479ac86b15ba2a55382b7694b2e68
Daniela Serafim <danyelaserafim@gmail.com>
2024-11-08 12:03:06 -0300

    corrigir model e fazer nova migration

--- academico/models/Ingresso.py
+++ academico/models/Ingresso.py
@@ -3,7 +3,7 @@ from django.db import models
 class Ingresso(models.Model):
     nome = models.CharField('Insira a forma de ingresso', max_length=250)
 
-    slug = models.SlugField(max_length=200, unique=True)
 
     def __str__(self):
         return self.nome
+    
